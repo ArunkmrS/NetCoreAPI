11c8dd18f44898e88b91ef543b246f3f1b2d92dc
Arun Kumar <arunkumars@live.in>
2020-04-01 15:52:36 +0530

    INIT

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/cities")]
+         public JsonResult GetCities()
+        {
+            return new JsonResult(
+
+                new List<Object>()
+                {
+                    new {Id=1, Name="Delhi"},
+                    new {Id=2, Name="Bangalore"}
+                });
+        }
+    }
+}
