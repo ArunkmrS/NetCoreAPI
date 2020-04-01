4f249642ca571d78f3e330661e43ea7f6aa312ca
Arun Kumar <arunkumars@live.in>
2020-04-01 16:09:07 +0530

    GET - allcities

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.InMemoryDS;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,13 @@ using System.Threading.Tasks;
 namespace CityInfo.API.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        [HttpGet("api/cities")]
+        [HttpGet()]
          public JsonResult GetCities()
         {
-            return new JsonResult(
-
-                new List<Object>()
-                {
-                    new {Id=1, Name="Delhi"},
-                    new {Id=2, Name="Bangalore"}
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
---
+++ CityInfo.API/InMemoryDS/CitiesDataStore.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.InMemoryDS
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto
+                {
+                    Id=1,
+                    Name="Delhi",
+                    Descrition="Capital of India"
+                },
+                new CityDto
+                {
+                    Id=2,
+                    Name="Bangalore",
+                    Descrition="Silicon Valley of India"
+                }
+            };
+        }
+    }
+}
